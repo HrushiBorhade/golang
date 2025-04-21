0c87943942fe4185c14484f02c76d33f95d41aa9
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-21 14:46:15 +0530

    melkey-fm-course/feat: Add variables and basic types

--- melkey-fm-course/main.go
+++ melkey-fm-course/main.go
@@ -6,4 +6,25 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
+
+	//Variables
+	var name string = "Hrushi"
+	fmt.Printf("Hey there! I am %s\n", name)
+
+	age := 27
+	fmt.Printf("I am %d years old\n", age)
+
+	var city string = "Pune"
+
+	country := "India"
+	fmt.Printf("I am from %s, %s\n", city, country)
+
+	//Zero Values
+	var defaultInt int
+	var defaultFloat float64
+	var defaultString string
+	var defaultBoolean bool
+
+	fmt.Printf("Default Value\n defaultInt: %d \n defaultFloat: %f \n defaultString: %s \n defaultBoolean: %t \n", defaultInt, defaultFloat, defaultString, defaultBoolean)
+
 }
