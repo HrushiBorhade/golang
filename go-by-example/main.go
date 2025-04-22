eee32daae9ab33754c1364cb176787639c6d1ee1
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-22 23:01:09 +0530

    go-by-example/feat: add error section

---
+++ go-by-example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	error_test "github.com/HrushiBorhade/golang/go-by-example/sections/error"
+)
+
+func main() {
+	fmt.Println("Go By Example!")
+	fmt.Println("-------------- Errors -------------")
+	error_test.Execute()
+	fmt.Println("-----------------------------------")
+}
