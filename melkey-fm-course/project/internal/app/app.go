b85133f74214775eaebec41a7ec9990a5991a4fb
HrushiBorhade <hrushiborhade123@gmail.com>
2025-05-01 00:58:08 +0530

    melkey-fm-course/feat: Add Goose Migrations

--- melkey-fm-course/project/internal/app/app.go
+++ melkey-fm-course/project/internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/HrushiBorhade/golang/melkey-fm-course/project/internal/api"
 	"github.com/HrushiBorhade/golang/melkey-fm-course/project/internal/store"
+	"github.com/HrushiBorhade/golang/melkey-fm-course/project/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,12 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		fmt.Printf("Error in app.go")
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
