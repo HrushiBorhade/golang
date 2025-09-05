f3203940d6815673bb74f4562b2246a7f50a72b9
HrushiBorhade <hrushiborhade123@gmail.com>
2025-09-05 18:04:16 +0530

    feat: go-by-example panic chapter

---
+++ hands-on/panic/go.mod
@@ -0,0 +1,3 @@
+module github.com/HrushiBorhade/golang/hands-on/panic
+
+go 1.24.2
---
+++ hands-on/panic/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+
+func main() {
+
+	panic("a problem")
+	if _,err := os.Create("/tmp/file"); err!=nil {
+		panic(err)
+	}
+}
